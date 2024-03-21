c8e96e3f077da5e8677750d6d32542ab6d639efc
annamariakoutsoras <annamariakoutsoras@Annamarias-MacBook-Air>
2024-03-21 09:50:16 +0000

    Added toogle

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,12 +1,35 @@
+'use client'
+
 import "./Header.css";
 import Link from "next/link";
-// import ClientComponent from './ClientComponent';
+import { useState } from 'react';
+import Image from "next/image";
+import hamburger from '/Images/menu-open-button.png'
+import hamburgerClosed from '/Images/menu-close-button.png'
 
 function Header() {
+
+    const [navbarOpen, setNavbarOpen] = useState(false);
+
+    const toggle = () => {
+      setNavbarOpen(!navbarOpen);
+     };
+
   return (
     <header className="site-header">
-      <h1 className="site-title">🔥 Fireplace Palace</h1>
-      <nav>
+<h1 className="site-title">🔥 Fireplace Palace</h1>
+
+ <nav className="navbar">
+        <button
+          className="toggle"
+          onClick={() => setNavbarOpen((prev) => !prev)}
+         >  
+
+          {navbarOpen ? <Image src={hamburgerClosed} alt="navclosed"
+         /> : <Image src={hamburger} alt="navopen"
+         />}
+</button> 
+
         <ul>
           <li>
             <Link href="/">Home</Link>
@@ -15,7 +38,7 @@ function Header() {
             <Link href="/founders">Meet the founders</Link>
           </li>
         </ul>
-      </nav>
+       </nav> 
     </header>
   );
 }
