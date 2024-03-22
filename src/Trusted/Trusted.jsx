1c192c39c73770c7d1ce2c83324e5f13bffde6de
annamariakoutsoras <annamariakoutsoras@Annamarias-MacBook-Air>
2024-03-22 09:33:28 +0000

    fetched api data

--- src/Trusted/Trusted.jsx
+++ src/Trusted/Trusted.jsx
@@ -1,18 +1,49 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import "./Trusted.css";
 
+
+
+
 function Trusted() {
+  const [country, setCountry] = useState("england");
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://seal-app-336e8.ondigitalocean.app/reviews?country=${country}`)
+      .then((response) => response.json())
+      .then((json) => {
+        const items = Array.isArray(json) ? json : [json];
+        console.log(items); 
+        setItems(items); 
+      });
+  }, [country]);
+  
+
   return (
     <section className="trusted">
       <h1>Trusted.</h1>
-      <p class="trusted-para">
-        We've got thousands of happy customers all over the UK. Choose your
+      <p className="trusted-para">
+        We have got thousands of happy customers all over the UK. Choose your
         country to see the latest review:
       </p>
       <div className="button-container">
-        <button type="button">England</button>
-        <button type="button">Wales</button>
-        <button type="button">Scotland</button>
+        <button onClick={() => setCountry("england")} type="button">England</button>
+        <button onClick={() => setCountry("wales")} type="button">Wales</button>
+        <button onClick={() => setCountry("scotland")} type="button">Scotland</button>
       </div>
+      {items.map((item, index) => {
+  console.log(item);
+  return (
+    <div key={index}>
+      <h2>{item.author}</h2>
+      <p>{item.text}</p>
+    </div>
+  );
+})}
+
+
     </section>
   );
 }
